cebf741dbac7fa14fa3e936e3c60b282b9fe3d12
Annania Plossl <nia.plossl@gmail.com>
2024-03-12 13:07:05 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(record) {
+    let win = record.find((element)=> element.result==="W");
+    if(typeof(win)==='object'){
+        win=win.year
+    }
+    return win;
+}
